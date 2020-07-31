ff3f4ef179569610d085c0e5d6bc725588606cff
Gstvdm <gstvdm@gmail.com>
2020-07-30 21:24:53 -0300

    Added player texture

--- scripts/Steve.gd
+++ scripts/Steve.gd
@@ -2,6 +2,8 @@ extends KinematicBody
 
 var velocity = Vector3(0,0,0)
 const SPEED = 15
+const ROTSPEED = PI/15
+
 func _ready():
 	pass
 
@@ -9,15 +11,19 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
+		$MeshInstance.rotate_z(-ROTSPEED)
 		
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
+		$MeshInstance.rotate_z(ROTSPEED)
 		
 	if Input.is_action_pressed("ui_up"):
 		velocity.z = -SPEED
+		$MeshInstance.rotate_x(-ROTSPEED)
 		
 	if Input.is_action_pressed("ui_down"):
 		velocity.z = SPEED
+		$MeshInstance.rotate_x(ROTSPEED)
 	
 	
 	
